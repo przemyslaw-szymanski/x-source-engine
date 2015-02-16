08880d1df75e5a60b91a0f4b8404f4bc2cb199ca
Przemek Szymanski <przemek.p.szymanski@gmail.com>
2015-02-16 17:17:45 +0100

    update

---
+++ Engine/include/XSECFile.h
@@ -0,0 +1,32 @@
+#ifndef XSE_CFILE_H
+#define XSE_CFILE_H
+
+#include "XSECommon.h"
+#include "XSTTCData.h"
+
+namespace XSE
+{
+	namespace Resources
+	{
+		class XST_API CFile : public XST::IObject
+		{
+			public:
+
+				typedef XST::TCData< u8 >	Data;
+
+			public:
+
+			protected:
+
+				Data		m_Data;
+				ul32		m_ulHandle;
+				ul32		m_ulGroupHandle;
+				ul32		m_ulInfoHandle;
+		};
+
+		typedef XST::TCObjectSmartPointer< CFile >	FilePtr;
+		typedef XST::TCWeakPointer< CFile >			FileWeakPtr;
+	}
+}
+
+#endif // XSE_CFILE_H
--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 git config push.default current
+git add Engine/*.h Engine/*.cpp
 git add -u ; git commit -m "update" ; git push
